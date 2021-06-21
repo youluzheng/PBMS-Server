28989899f6de85112a3eeaa252e39a2bddf190c7
Redamancy <758249101@qq.com>
2021-06-21 22:32:30 +0800

    feature: picture upload interface (#6)
    feature：picture upload interface #1
    close #1
    
    Co-authored-by: wangjun <wangjun@echaincity.com>
    Reviewed-on: https://gitea.965.life/PBMS/PBMS-server/pulls/6
    Reviewed-by: zyl <zyl@965.life>
    Reviewed-by: Ju <zqs@965.life>
    Co-authored-by: Redamancy <758249101@qq.com>
    Co-committed-by: Redamancy <758249101@qq.com>

---
+++ src/main/java/org/pbms/pbmsserver/init/Init.java
@@ -0,0 +1,33 @@
+package org.pbms.pbmsserver.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.pbms.pbmsserver.common.GlobalConstant;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+/**
+ * 创建初始文件夹
+ *
+ * @author 王俊
+ * @Date 2021-06-21
+ */
+@Slf4j
+@Component
+public class Init implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("检查{}是否存在...", GlobalConstant.ROOT_PATH);
+
+        File rootPath = new File(GlobalConstant.ROOT_PATH);
+        if (!rootPath.exists()){
+            log.info("{}不存在, 创建中...", GlobalConstant.ROOT_PATH);
+            rootPath.mkdirs();
+            log.info("{}创建完成", GlobalConstant.ROOT_PATH);
+            return ;
+        }
+        log.info("{}已经存在", GlobalConstant.ROOT_PATH);
+    }
+}
