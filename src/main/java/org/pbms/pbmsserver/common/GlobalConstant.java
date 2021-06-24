1e766332583350e60cd048d50b16d3795ad6f18b
zyl <zyl@965.life>
2021-06-24 06:56:51 +0000

    chore:修改配置文件从环境变量读取

--- src/main/java/org/pbms/pbmsserver/common/GlobalConstant.java
+++ src/main/java/org/pbms/pbmsserver/common/GlobalConstant.java
@@ -3,25 +3,30 @@ package org.pbms.pbmsserver.common;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author zyl
  * @Date 2021-06-21
  */
 
 @Component
+@Slf4j
 public class GlobalConstant {
 
     public static String ROOT_PATH;
 
     @Value("${picturebed.rootpath:pictureBed}")
-    public void setRootPath(String rootPath){
+    public void setRootPath(String rootPath) {
+        log.info("set rootPath:{}", rootPath);
         GlobalConstant.ROOT_PATH = rootPath;
     }
 
     public static String BASEURL;
 
     @Value("${picturebed.baseurl:http://127.0.0.1:8080}")
-    public void setBaseurl(String baseurl){
+    public void setBaseurl(String baseurl) {
+        log.info("set baseurl:{}", baseurl);
         GlobalConstant.BASEURL = baseurl;
     }
 }
