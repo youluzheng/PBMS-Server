fbc9759164c8001a015317bf284475a9353df719
zyl <youluzheng@foxmail.com>
2021-10-11 22:09:57 +0800

    test&fix: 添加responseProcessor测试用例

--- src/main/java/org/pbms/pbmsserver/service/lifecycle/after/ResponseProcessor.java
+++ src/main/java/org/pbms/pbmsserver/service/lifecycle/after/ResponseProcessor.java
@@ -3,11 +3,11 @@ package org.pbms.pbmsserver.service.lifecycle.after;
 import cn.hutool.core.io.file.FileNameUtil;
 import org.pbms.pbmsserver.repository.model.UserSettings;
 import org.pbms.pbmsserver.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 返回格式处理
@@ -16,9 +16,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Component
 public class ResponseProcessor {
-
-    @Autowired
-    private HttpServletRequest request;
+    private static final Logger log = LoggerFactory.getLogger(ResponseProcessor.class);
 
     @Autowired
     private UserService userService;
@@ -27,9 +25,12 @@ public class ResponseProcessor {
         UserSettings userSettings = this.userService.getSettings();
         if ("url".equals(userSettings.getResponseReturnType())) {
             return url;
-        } else {
-            String imageName = FileNameUtil.extName(image.getOriginalFilename());
+        } else if ("markdown".equals(userSettings.getResponseReturnType())) {
+            String imageName = FileNameUtil.mainName(image.getOriginalFilename());
             return "![" + imageName + "](" + url + ")";
+        } else {
+            log.warn("异常responseType:{}, 默认返回url", userSettings.getResponseReturnType());
+            return url;
         }
     }
 }
---
+++ src/test/java/org/pbms/pbmsserver/service/lifecycle/after/ResponseProcessorTest.java
@@ -0,0 +1,54 @@
+package org.pbms.pbmsserver.service.lifecycle.after;
+
+
+import cn.hutool.core.io.file.FileNameUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.pbms.pbmsserver.repository.model.UserSettings;
+import org.pbms.pbmsserver.service.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class ResponseProcessorTest {
+
+    @InjectMocks
+    ResponseProcessor responseProcessor;
+
+    @Mock
+    UserService userService;
+
+    MultipartFile image = new MockMultipartFile("test.png", "test.png", MediaType.IMAGE_PNG.getType(), new byte[]{});
+
+    String url = "http://127.0.0.1/image";
+
+    UserSettings getUserSettings(String type) {
+        UserSettings userSettings = new UserSettings();
+        userSettings.setUserId(1L);
+        userSettings.setResponseReturnType(type);
+        return userSettings;
+    }
+
+    @Test
+    void testResponseHandler_url() {
+        doReturn(this.getUserSettings("url")).when(this.userService).getSettings();
+        String expected = this.url;
+        String actual = responseProcessor.responseHandler(this.image, url);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testResponseHandler_markdown() {
+        doReturn(this.getUserSettings("markdown")).when(this.userService).getSettings();
+        String expected = "![" + FileNameUtil.mainName(this.image.getOriginalFilename()) + "](" + this.url + ")";
+        String actual = responseProcessor.responseHandler(this.image, url);
+        assertEquals(expected, actual);
+    }
+}
