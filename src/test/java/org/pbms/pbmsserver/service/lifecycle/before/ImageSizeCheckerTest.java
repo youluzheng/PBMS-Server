b150e86ab4c9a2b6c417976855b66924ba7c869b
zyl <youluzheng@foxmail.com>
2021-10-17 21:17:37 +0800

    test: 修改文件大小测试用例

--- src/test/java/org/pbms/pbmsserver/service/lifecycle/before/ImageSizeCheckerTest.java
+++ src/test/java/org/pbms/pbmsserver/service/lifecycle/before/ImageSizeCheckerTest.java
@@ -1,12 +1,12 @@
 package org.pbms.pbmsserver.service.lifecycle.before;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.pbms.pbmsserver.common.constant.ConfigReader;
+import org.pbms.pbmsserver.common.constant.ServerConstant;
 import org.pbms.pbmsserver.common.exception.BusinessException;
 import org.pbms.pbmsserver.common.exception.BusinessStatus;
 import org.slf4j.Logger;
@@ -26,10 +26,16 @@ class ImageSizeCheckerTest {
     ImageSizeChecker imageSizeChecker;
 
     final String fileName = "test.png";
-    
+
+    long maxSize;
+
     @BeforeEach
     void setup() {
         ConfigReader.configReader.setMaxSize("1KB");
+        this.maxSize = ServerConstant.SERVER_MAX_SIZE;
+        if (this.maxSize == 0) {
+            fail("异常文件大小");
+        }
     }
 
 
@@ -37,10 +43,9 @@ class ImageSizeCheckerTest {
         return new MockMultipartFile("image", this.fileName, MediaType.IMAGE_PNG.getType(), data);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1025, 10250})
-    void checkImageSize_size_too_large(int size) {
-        MockMultipartFile file = this.generateFile(new byte[size]);
+    @Test
+    void checkImageSize_size_too_large() {
+        MockMultipartFile file = this.generateFile(new byte[(int) this.maxSize + 1]);
         try {
             this.imageSizeChecker.checkImageSize(file);
             fail("未抛出指定异常");
@@ -49,10 +54,9 @@ class ImageSizeCheckerTest {
         }
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 1024})
-    void checkImageSize_success(int size) {
-        MockMultipartFile file = this.generateFile(new byte[size]);
+    @Test
+    void checkImageSize_success() {
+        MockMultipartFile file = this.generateFile(new byte[(int) this.maxSize]);
         this.imageSizeChecker.checkImageSize(file);
     }
 }
